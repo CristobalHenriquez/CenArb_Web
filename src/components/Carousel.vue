00c50f729e5d7c53b78c12df3ddae72d73c9254d
NaachoRod <ignaciorod.dev@gmail.com>
2025-01-09 19:06:13 -0300

    agrego login, modifico home y responsive de footer

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,37 +1,50 @@
 <script setup>
-    import { ref } from 'vue';
+import { ref } from 'vue';
 
-    defineProps({
-        items: { type: Array, required: true },
-        bgColor: { type: String, default: '#26473c' },
-        txColor: { type: String, default: '#91a19c' },
-        txAlign: { type: String, default: 'center' },
-        pL: {type: String, default: 'p-0'},
-        pR: {type: String, default: 'p-0'}
-    });
+defineProps({
+  items: { type: Array, required: true },
+  bgColor: { type: String, default: '#26473c' },
+  hvColor: { type: String, default: '#3c6354' },
+  txColor: { type: String, default: '#91a19c' },
+  hvTxColor: { type: String, default: '#3c6354' },
+  txAlign: { type: String, default: 'center' },
+  pL: { type: String, default: 'p-0' },
+  pR: { type: String, default: 'p-0' }
+});
 
-    const carousel = ref(null);
-    const isDragging = ref(false);
-    const startX = ref(0);
-    const scrollLeft = ref(0);
+const carousel = ref(null);
+const isDragging = ref(false);
+const startX = ref(0);
+const scrollLeft = ref(0);
 
- 
-    const startDrag = (e) => {
-        isDragging.value = true;
-        startX.value = e.pageX - carousel.value.offsetLeft;
-        scrollLeft.value = carousel.value.scrollLeft;
-    };
+// Almacena el índice del elemento sobre el cual está el mouse
+const hoveredIndex = ref(null);
 
-    const onDrag = (e) => {
-        if (!isDragging.value) return;
-        const x = e.pageX - carousel.value.offsetLeft;
-        const walk = (x - startX.value) * 2;
-        carousel.value.scrollLeft = scrollLeft.value - walk;
-    };
+const startDrag = (e) => {
+  isDragging.value = true;
+  startX.value = e.pageX - carousel.value.offsetLeft;
+  scrollLeft.value = carousel.value.scrollLeft;
+};
 
-    const stopDrag = () => {
-        isDragging.value = false;
-    };
+const onDrag = (e) => {
+  if (!isDragging.value) return;
+  const x = e.pageX - carousel.value.offsetLeft;
+  const walk = (x - startX.value) * 2;
+  carousel.value.scrollLeft = scrollLeft.value - walk;
+};
+
+const stopDrag = () => {
+  isDragging.value = false;
+};
+
+//Hover
+const handleMouseEnter = (index) => {
+  hoveredIndex.value = index; 
+};
+
+const handleMouseLeave = () => {
+  hoveredIndex.value = null; 
+};
 </script>
 
 <template>
@@ -46,17 +59,22 @@
     <div 
       v-for="(item, index) in items" 
       :key="index" 
-      :style="{ backgroundColor: bgColor, userSelect: 'none' }"
+      :style="{ 
+        backgroundColor: hoveredIndex === index ? hvColor : bgColor,
+        userSelect: 'none' 
+      }"
       class="flex-shrink-0 w-auto h-36 rounded-3xl shadow-inner-top flex flex-col justify-center snap-start"
+      @mouseenter="handleMouseEnter(index)"
+      @mouseleave="handleMouseLeave"
     >
-      <div class="w-auto px-5">
-        <h5 class="text-white text-4xl font-bold" :style="{ textAlign: txAlign }">{{items.length}}</h5>
-        <h4 class="text-3xl font-semibold" :style="{ color: txColor, textAlign: txAlign }">{{item.name}}</h4>
+      <div class="w-auto px-5"
+      @mouseenter="handleMouseEnter(index)"
+      @mouseleave="handleMouseLeave"
+      >
+        <h5 class="text-white text-4xl font-bold" :style="{ textAlign: txAlign, color: hoveredIndex === index ? hvTxColor : color }">{{ items.length }}</h5>
+        <h4 class="text-3xl font-semibold" :style="{ color: txColor, textAlign: txAlign }">{{ item.name }}</h4>
         <h3 class="text-3xl font-semibold" :style="{ color: txColor, textAlign: txAlign }">{{ item.prov }}</h3>
       </div>
     </div>
   </div>
 </template>
-
-  
-  
