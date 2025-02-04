5ef5816cdcbdcee523f270a61c71204d1054a885
MartinCillo1989 <cillomartin.89@gmail.com>
2025-02-04 12:06:23 -0300

    Corregido boton salir del home y login

--- src/App.vue
+++ src/App.vue
@@ -15,9 +15,9 @@
 
    
     <div class="header-content">
-      <!-- Botón "Salir" solo si la ruta actual NO es 'home' -->
+      <!-- Botón "Salir" solo si la ruta actual NO es 'home' ni 'login' -->
       <RouterLink 
-        v-if="route.name !== 'home'"
+        v-if="route.name !== 'home' && route.name !== 'login'"
         to="/" 
         class="block rounded-full bg-red-700 hover:bg-red-500 py-2 px-3 text-white text-center uppercase font-bold shadow text-sm">
         Salir
