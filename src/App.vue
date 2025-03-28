9db10e4578f97bbe3f9ac5424124d69e57dd876c
Cristobal Henriquez <cristobalhb@live.com>
2025-03-28 00:13:23 -0300

    actualizo redes

--- src/App.vue
+++ src/App.vue
@@ -88,16 +88,16 @@ window.addEventListener("storage", () => {
     COMPROMETIDOS CON LA ACCIÓN CLIMÁTICA
   </p>
   <div class="flex space-x-4">
-    <a href="https://www.linkedin.com" target="_blank">
+    <a href="https://ar.linkedin.com/company/ramcc-ar" target="_blank">
       <img src="./components/icons/Linke_ico.svg" class="h-8 w-8">
     </a>
-    <a href="https://www.instagram.com" target="_blank">
+    <a href="https://www.instagram.com/ramcc_ar/?hl=es" target="_blank">
       <img src="./components/icons/Insta_ico.svg" class="h-8 w-8">
     </a>
-    <a href="https://www.youtube.com" target="_blank">
+    <a href="https://www.youtube.com/@RAMCC_ar" target="_blank">
       <img src="./components/icons/You_ico.svg" class="h-8 w-8">
     </a>
-    <a href="https://www.facebook.com" target="_blank">
+    <a href="https://www.facebook.com/ramcc.net/?locale=cx_PH" target="_blank">
       <img src="./components/icons/Face_ico.svg" class="h-8 w-8">
     </a>
   </div>
